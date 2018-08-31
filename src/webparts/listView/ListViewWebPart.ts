eee876d949824865326aab44d19db6598761b0d1
Michal Adam <michalaizdebski@outlook.com>
2018-08-31 15:00:40 +0200

    property pane exemplar fields

--- src/webparts/listView/ListViewWebPart.ts
+++ src/webparts/listView/ListViewWebPart.ts
@@ -1,24 +1,28 @@
+import { Version } from '@microsoft/sp-core-library';
+import { BaseClientSideWebPart, IPropertyPaneConfiguration, PropertyPaneCheckbox, PropertyPaneDropdown, PropertyPaneTextField, PropertyPaneToggle } from '@microsoft/sp-webpart-base';
+import * as strings from 'ListViewWebPartStrings';
 import * as React from 'react';
 import * as ReactDom from 'react-dom';
-import { Version } from '@microsoft/sp-core-library';
-import {
-  BaseClientSideWebPart,
-  IPropertyPaneConfiguration,
-  PropertyPaneTextField
-} from '@microsoft/sp-webpart-base';
-
-import * as strings from 'ListViewWebPartStrings';
-import ListView from './components/ListView';
 import { IListViewProps } from './components/IListViewProps';
+import ListView from './components/ListView';
+
 
 export interface IListViewWebPartProps {
   description: string;
 }
 
+export interface IHelloWorldWebPartProps {
+  description: string;
+  test: string;
+  test1: boolean;
+  test2: string;
+  test3: boolean;
+}
+
 export default class ListViewWebPart extends BaseClientSideWebPart<IListViewWebPartProps> {
 
   public render(): void {
-    const element: React.ReactElement<IListViewProps > = React.createElement(
+    const element: React.ReactElement<IListViewProps> = React.createElement(
       ListView,
       {
         description: this.properties.description
@@ -47,14 +51,34 @@ export default class ListViewWebPart extends BaseClientSideWebPart<IListViewWebP
             {
               groupName: strings.BasicGroupName,
               groupFields: [
-                PropertyPaneTextField('description', {
-                  label: strings.DescriptionFieldLabel
-                })
-              ]
+              PropertyPaneTextField('description', {
+                label: 'Description'
+              }),
+              PropertyPaneTextField('test', {
+                label: 'Multi-line Text Field',
+                multiline: true
+              }),
+              PropertyPaneCheckbox('test1', {
+                text: 'Checkbox'
+              }),
+              PropertyPaneDropdown('test2', {
+                label: 'Dropdown',
+                options: [
+                  { key: '1', text: 'One' },
+                  { key: '2', text: 'Two' },
+                  { key: '3', text: 'Three' },
+                  { key: '4', text: 'Four' }
+                ]}),
+              PropertyPaneToggle('test3', {
+                label: 'Toggle',
+                onText: 'On',
+                offText: 'Off'
+              })
+            ]
             }
           ]
         }
       ]
     };
-  }
+   }
 }
